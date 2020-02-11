2e5ed52747db22789f4a4a64434a8ff6e802a2c7
Heriberto Figueroa Michel <hfigueroa@ucol.mx>
2020-02-10 23:45:46 -0600

    configuraciones multimples videos

--- CLASE 2/EventMultimedia/script.js
+++ CLASE 2/EventMultimedia/script.js
@@ -1,11 +1,32 @@
+
+const Videos = [
+"video/IngresaVideo.mp4",
+"video/IngresarVideo1.mp4",
+"video/IngresarVideo2.mp4",
+"video/IngresarVideo3.mp4",
+]
+
 const playB = document.getElementById('Play')
 const pauseB = document.getElementById("Pause")
 const video = document.getElementById("video")
 const aD = document.getElementById("ad")
 const pB1 = document.getElementById("add")
+const pib = document.getElementById("videoSelect")
+
 const muted = document.getElementById("muted")
 const salto = document.getElementById('salto')
 const saltoatras = document.getElementById('saltonegativo')
+
+let VC = 0
+pib.addEventListener("change", e=>{
+  VC = e.target.value
+})
+const visualizar =()=>{
+  video.src = Videos[VC]
+  video.load()
+}
+visualizar()
+pib.addEventListener("change",visualizar)
 
 saltoatras.addEventListener("click",()=>{
   x=video.currentTime;
